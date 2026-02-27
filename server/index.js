46766f2c22473c8d7cc6d34bbdd0f8d2005ea7c7
Sainath <sainath@nothing.attdns.com>
2026-02-27 21:03:02 +0530

    Fix missing imports in server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
 const mongoose = require('mongoose');
 
 const app = express();
